837d8428614dcea3b830d5e1684dafe8dd4206d8
James Halliday <mail@substack.net>
2014-01-29 04:55:03 -0800

    update the example

--- example/insert.js
+++ example/insert.js
@@ -3,8 +3,7 @@ var bpack = require('browser-pack');
 var insert = require('../');
 
 var files = [ __dirname + '/files/main.js' ];
-mdeps(files)
-    .pipe(insert(files))
+mdeps(files, { transform: insert })
     .pipe(bpack({ raw: true }))
     .pipe(process.stdout)
 ;
